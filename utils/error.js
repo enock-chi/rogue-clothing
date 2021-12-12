6e17f74498782941c6fc4785cd2b3ba8155dfb1d
Enock Chikobvu <sechikobvu@gmail.com>
2021-12-12 18:04:02 +0200

    attempt 3

--- utils/error.js
+++ utils/error.js
@@ -1,9 +1,10 @@
 import db from './db';
 
-const getError = (err) =>
+const getError = (err) => {
   err.response && err.response.data && err.response.data.message
     ? err.response.data.message
     : err.message;
+};
 
 const onError = async (err, req, res, next) => {
   await db.disconnect();
