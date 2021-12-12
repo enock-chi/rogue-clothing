56ce2f69a7dc1aa9076b0c245fee4c5f482cf72f
Enock Chikobvu <sechikobvu@gmail.com>
2021-12-12 19:22:52 +0200

    env

--- utils/onerror.js
+++ utils/onerror.js
@@ -4,4 +4,4 @@ const onError = async (err, req, res, next) => {
   await db.disconnect();
   res.status(500).send({ message: err.toString() });
 };
-export default { onError };
+export default onError;
